3cc41a79a37b21aca88d9588a5f84e737e89224d
Your Name <you@example.com>
2022-09-12 15:57:20 +0300

    Save

---
+++ HW6/41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+# nullable disable
+int[] FillArray()
+{
+    Console.WriteLine("Введите числа через запятую: ");
+    string number = Console.ReadLine();
+    string[] array = number.Split(",");
+    int[] newArray = new int[array.Length];
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = Convert.ToInt32(array[i]);
+    }
+    return newArray;
+}
+
+int CountNumberInArray(int[] newArray)
+{
+    int result = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] > 0) result++;
+    }
+    return result;
+}
+
+void PrintResult(int result)
+{
+    Console.WriteLine($"-----> {result}");
+}
+
+PrintResult(CountNumberInArray(FillArray()));
+
---
+++ HW6/43/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double[] CalculateResult(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    double[] array = new double[2];
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+
+void PrintResult(double[] array)
+{
+    Console.Write($"Точка пересечения (");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write($"{array[i]}; ");
+        }
+        else
+        {
+            Console.WriteLine($"{array[i]})");
+        }
+    }
+}
+
+Console.Write("b1 = ");
+bool isNumB1 = double.TryParse(Console.ReadLine(), out double b1);
+Console.Write("k1 = ");
+bool isNumK1 = double.TryParse(Console.ReadLine(), out double k1);
+Console.Write("b2 = ");
+bool isNumB2 = double.TryParse(Console.ReadLine(), out double b2);
+Console.Write("k2 = ");
+bool isNumK2 = double.TryParse(Console.ReadLine(), out double k2);
+
+if (!isNumB1 || !isNumK1 || !isNumB2 || !isNumK2)
+{
+    Console.WriteLine("Попробуй ещё раз ");
+    return;
+}
+if (k1 - k2 == 0)
+{
+    Console.WriteLine("Прямые параллельны и не пересекаются");
+    return;
+}
+double[] result = CalculateResult(b1, k1, b2, k2);
+PrintResult(result);
